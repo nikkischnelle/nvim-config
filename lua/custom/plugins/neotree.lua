386df3d278575c94c680ec86bba71f280e5d72c4
nikkitschierske <nikki@schnelle.dev>
2024-08-31 11:08:48 +0200

    add base-plugins

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,28 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+    '3rd/image.nvim',
+  },
+  keys = {
+    { '<leader>f', '<cmd>Neotree toggle<cr>', desc = 'NeoTree' },
+    { 'f', '<cmd>Neotree toggle<cr>', desc = 'NeoTree' },
+  },
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+        },
+      },
+    }
+  end,
+}
