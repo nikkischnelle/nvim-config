45ca68dee47e62c1cf85b811edfeb64a9cf9746e
nikkitschierske <nikki@schnelle.dev>
2024-10-03 14:59:14 +0200

    add language servers

--- init.lua
+++ init.lua
@@ -608,11 +608,27 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         pyright = {},
-        rust_analyzer = {},
-        ansiblels = {},
         ruff = {},
-        cssls = {},
-        jsonls = {},
+        rust_analyzer = {},
+        jdtls = {},
+        bashls = {},
+        isort = {},
+        jq = {},
+        markdownlint = {},
+        prettierd = {},
+        yamllint = {},
+        yamlfmt = {},
+        xmlformatter = {},
+
+        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        --
+        -- Some languages (like typescript) have entire language plugins that can be useful:
+        --    https://github.com/pmizio/typescript-tools.nvim
+        --
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        -- ts_ls = {},
+        --
+
         lua_ls = {
           settings = {
             Lua = {
